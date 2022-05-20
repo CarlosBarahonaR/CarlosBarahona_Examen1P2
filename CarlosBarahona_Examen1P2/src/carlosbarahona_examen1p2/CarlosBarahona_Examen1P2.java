94974dbb496a8971b2026d0469082ecd858d7686
CJBarahona <cbarahona@t-developing.com>
2022-05-20 14:28:35 -0600

    Menu

--- CarlosBarahona_Examen1P2/src/carlosbarahona_examen1p2/CarlosBarahona_Examen1P2.java
+++ CarlosBarahona_Examen1P2/src/carlosbarahona_examen1p2/CarlosBarahona_Examen1P2.java
@@ -5,6 +5,9 @@
  */
 package carlosbarahona_examen1p2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -16,6 +19,37 @@ public class CarlosBarahona_Examen1P2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<PC> pc = new ArrayList();
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 3) {
+            System.out.println("Bienvenido al sistema de administrador de redes\n"
+                    + "¿Qué desea hacer?\n"
+                    + "1) CRUD de PC\n"
+                    + "2) Ingresar PC\n"
+                    + "3) Salir");
+            opcion = leer.nextInt();
+            switch (opcion) {
+                case 1: {
+                    int opcion2 = 0;
+                    while (opcion2 != 5) {
+                        System.out.println("Bienvenido al CRUD\n"
+                                + "¿Qué desea hacer?\n"
+                                + "1) Crear PC\n"
+                                + "2) Modificar PC\n"
+                                + "3) Listar PC\n"
+                                + "4) Eliminar PC\n"
+                                + "5) Salir");
+                        opcion2 = leer.nextInt();
+                        switch (opcion2) {
+                            case 1: {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    
+
 }
